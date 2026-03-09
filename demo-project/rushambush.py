166d481182a1ad67fed36c0c7748511da241b2b6
zaafirrahman <zaafir123rahman@gmail.com>
2026-03-09 23:25:17 +0700

    ghj

---
+++ demo-project/rushambush.py
@@ -0,0 +1,5 @@
+from skala import *
+
+rush = sk(6,10000000)
+
+print(rush)
---
+++ demo-project/skala.py
@@ -0,0 +1,11 @@
+def sk(jp,js):
+    hasil = jp / js
+    return hasil
+
+def js(jp,sk):
+    hasil = jp / sk
+    return hasil
+
+def jp(sk,js):
+    hasil = sk * js
+    return hasil
